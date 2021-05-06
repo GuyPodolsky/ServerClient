2d5cf859554887f9c895592b2a2ab0f139050f2f
guypodolsky <guy.podolsky@outlook.com>
2021-05-07 02:09:40 +0300

    hi hi

---
+++ src/Client.java
@@ -0,0 +1,18 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+
+public class Client {
+    DataInputStream dis = new DataInputStream(s.getInputStream());
+    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+    ServerSocket ss = new ServerSocket(1234);
+
+    Socket s = ss.accept();
+
+    While(true)
+    {
+
+    }
+
+}
